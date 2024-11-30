5974ade6da2d8c24c0d32a466a923010e92092d7
locmark <kkraczyna@gmail.com>
2024-11-30 01:11:13 +0100

    secrets..

--- include/secrets.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-constexpr char* wifiSSID = "Misiowo";
-constexpr char* wifiPassword = "zuziauciekla";
-
-constexpr char* mqttServer = "192.168.0.60";
-constexpr char* mqttUser = "misiowo";
-constexpr char* mqttPassword = "zuziauciekla";
